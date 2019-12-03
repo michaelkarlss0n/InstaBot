af7b15a6693c33f970f5039422f95c32805d17f5
Pratik Das <49571663+DasPratikpd97@users.noreply.github.com>
2019-12-03 22:24:37 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The example.py is deprecated. Please use the declarative mode with YAML settings
+Please refer to Github README + (instabot -h) + Blog section for further information
+"""
+print(__doc__)
